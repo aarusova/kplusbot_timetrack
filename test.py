5c51aba7bb6fb8193c7dc6bb5c5a493ffc0a8522
aarusova <aarusova@ya.ru>
2025-06-05 23:06:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,20 @@
-import os
-import re
-import logging
-from datetime import datetime, timedelta
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    MessageHandler,
-    CallbackQueryHandler,
-    ContextTypes,
-    ConversationHandler,
-    filters,
-    ApplicationBuilder,
-    TypeHandler
-)
-import json
-from tempfile import NamedTemporaryFile
-from flask import Flask, jsonify
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "7635522928:AAEHZ8LkGtxuHAw87qaiBIstnhPLQq1HBbs"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("TEST OK")
+    await update.message.reply_text("Бот работает! ✅")
 
 def main():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.run_webhook(listen="0.0.0.0", port=10000)
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=10000,
+        webhook_url=f"https://kplusbot-timetrack.onrender.com/{TOKEN}"
+    )
+
+if __name__ == "__main__":
+    main()
